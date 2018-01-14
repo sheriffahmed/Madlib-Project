7db0f79ccf215da4588fe3e5b47dd4074be13542
StephanieHou <32886693+StephanieHou@users.noreply.github.com>
2018-01-14 15:01:40 -0500

    Add

--- src/madlibAPI.js
+++ src/madlibAPI.js
@@ -1,29 +1,72 @@
 import axios from "axios";
 
-const nouns = [
-  "ball",
-  "bat",
-  "bed",
-  "book",
-  "boy",
-  "bun",
-  "can",
-  "cake",
-  "cap",
-  "car",
-  "cat",
-  "cow",
-  "cub",
-  "cup",
-  "dad",
-  "day",
-  "dog",
-  "doll",
-  "dust",
-  "fan"
-];
+const wordList = {
+  nouns: [
+    "ball",
+    "bat",
+    "bed",
+    "book",
+    "boy",
+    "bun",
+    "can",
+    "cake",
+    "cap",
+    "car",
+    "cat",
+    "cow",
+    "cub",
+    "cup",
+    "dad",
+    "day",
+    "dog",
+    "doll",
+    "dust",
+    "fan"
+  ],
+  verbs: [
+    "bring",
+    "send",
+    "owe",
+    "contain",
+    "buy",
+    "show",
+    "take",
+    "tell",
+    "verify",
+    "check",
+    "get",
+    "wash",
+    "finalize",
+    "annoy",
+    "lay",
+    "lend",
+    "offer",
+    "edit",
+    "make",
+    "phone"
+  ],
+  abjectives: [
+    "adorable",
+    "beautiful",
+    "clean",
+    "drab",
+    "elegant",
+    "fancy",
+    "glamorous",
+    "handsome",
+    "long",
+    "magnificent",
+    "old-fashioned",
+    "plain",
+    "quaint",
+    "sparkling",
+    "ugliest",
+    "unsightly"
+  ]
+};
 
-const getAll = () => nouns;
+const getNouns = () => wordList.nouns;
+const getVerbs = () => wordList.verbs;
 
 // const getRandomWords = () => axios.get("http://madlibz.herokuapp.com/api/random?minlength=2&maxlength=5");
 
@@ -31,5 +74,6 @@ const getAll = () => nouns;
 //   axios.get(`http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=${word}`);
 
 export default {
-  getAll
+  getVerbs,
+  getNouns
 };
