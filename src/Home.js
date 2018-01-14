0d84f27129343aa3d6373d5039fc9a7be90ddac3
StephanieHou <32886693+StephanieHou@users.noreply.github.com>
2018-01-14 11:31:30 -0500

    Scaldfold

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h2>Madlib Central</h2>
+  </div>
+);
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
+import App from "./App";
 
 render(
   <BrowserRouter>
